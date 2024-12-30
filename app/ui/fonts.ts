25d5a9528f77cb01e50b2c6e4df143b751a7a052
Tyler Abbott <tabbott85@gmail.com>
2024-12-29 22:01:04 -0600

    font subset

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
 import { Inter, Poppins } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
-export const poppins = Poppins({ weight: '900' });
+export const poppins = Poppins({ subsets: ['latin'], weight: '900' });
